7022a7ff67ddfefcc580add689187bdbd367fbcc
blake S <design.harls@icloud.com>
2023-02-08 23:12:31 +0100

    request get show console

---
+++ Mini-proyect-Axios/src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { onMounted } from "vue";
+import axios from "axios";
+
+const characters = [];
+const onSubmit = async () => {
+  const res = await axios.get("https://rickandmortyapi.com/api/character/");
+  
+  characters.push(...res.data.results);
+  console.log(res.data.results);
+};
+
+onMounted(onSubmit);
+characters;
+
+
+
+
+
+</script>
+
+<template>
+
+
+
+  
+</template>
